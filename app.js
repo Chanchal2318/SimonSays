586541bbb9d43cabd25d3408a5824054242f60ae
Chanchal2318 <133849770+Chanchal2318@users.noreply.github.com>
2024-04-27 13:13:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// let h2=document.querySelector("h2");
+// console.dir(h2.innerText);
+// h2.innerText=h2.innerText+" from chanchal";
+// // h2.innerText="Hello Javascript from Chanchal";
+// let res=document.querySelectorAll(".box");
+// res[0].innerText="first div";
+// res[1].innerText="second div";
+// res[2].innerText="third div";
+
+// let newbtn=document.createElement("button");
+// newbtn.innerText="Click Me";
+// newbtn.style.backgroundColor = "red";
+// newbtn.style.color = "white";
+// document.querySelector("body").prepend(newbtn);
+
+// let p=document.querySelector("p");
+// console.log(p);
+// // p.setAttribute("new","myclass");
+// p.classList.add("myclass");
+// // p.classList.remove("myclass");
+
+// let newbtn=document.querySelector(".newbtn");
+// console.log(newbtn);
+
+// newbtn.ondblclick=()=>{
+//     backgroundColor="green";
+//     // console.log("Btn was clicked twice")
+// };
+
+// Toggle button
+
+// let modebtn=document.querySelector("#mode");
+// let currMode = "light";
+// modebtn.addEventListener("click",()=>{
+//     if(currMode === "light"){
+//         currMode = "dark";
+//         document.querySelector("body").style.backgroundColor = "black";
+//     }
+//     else{
+//         currMode = "light";
+//         document.querySelector("body").style.backgroundColor = "white";
+//     }
+// })
